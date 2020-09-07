57190618b883c784573c841a5904944d4ee0a42d
Mariusz Wójcik <36dragan@gmail.com>
2020-09-07 22:45:06 +0200

    add layout

--- Picture/Form1.Designer.cs
+++ Picture/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btChoose = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btChoose
+            // 
+            this.btChoose.Location = new System.Drawing.Point(12, 12);
+            this.btChoose.Name = "btChoose";
+            this.btChoose.Size = new System.Drawing.Size(96, 23);
+            this.btChoose.TabIndex = 0;
+            this.btChoose.Text = "Wybierz zdjęcie";
+            this.btChoose.UseVisualStyleBackColor = true;
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(114, 12);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(97, 23);
+            this.btClear.TabIndex = 1;
+            this.btClear.Text = "Wyczyść zdjęcie";
+            this.btClear.UseVisualStyleBackColor = true;
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(12, 41);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(776, 397);
+            this.pbMain.TabIndex = 2;
+            this.pbMain.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btChoose);
+            this.Name = "Form1";
+            this.Text = "Zdjęcie";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btChoose;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.PictureBox pbMain;
     }
 }
 
