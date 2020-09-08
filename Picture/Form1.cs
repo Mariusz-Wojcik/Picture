f4e47ceae7f9f9daf57b1b7ecd277c7343e10da1
Mariusz Wójcik <36dragan@gmail.com>
2020-09-08 12:17:47 +0200

    add Clear Button function

--- Picture/Form1.cs
+++ Picture/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Picture.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,20 +17,29 @@ namespace Picture
         public Form1()
         {
             InitializeComponent();
+            PicturePresent();
         }
 
+        private void PicturePresent()
+        {
+            if (filePath != "")
+                btClear.Enabled = true;
+            else
+                btClear.Enabled = false;
+        }
         private void btChoose_Click(object sender, EventArgs e)
         {
             var openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == DialogResult.OK)
                 filePath = openFileDialog.FileName;
-            //MessageBox.Show($"{filePath}");
             pbMain.Image = Image.FromFile(filePath);
+            btClear.Enabled = true;
         }
 
-            private void btClear_Click(object sender, EventArgs e)
+        private void btClear_Click(object sender, EventArgs e)
         {
-
+            pbMain.Image = null;
+            btClear.Enabled = false;
         }
     }
 }
